ce6f147eefa847ffbd83fafeefdcd03ec01ffc9f
EC2 Default User <cae-10@hotmail.com>
2021-06-27 08:15:00 +0000

    Implemented method for uploading images to s3 and return uri.

--- src/pet.py
+++ src/pet.py
@@ -1,13 +1,15 @@
 import json
 import boto3
+import base64
 import os
-
+import uuid
 
 animal_adoption_table= os.environ['ADOPTION_TABLE']
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(animal_adoption_table)
-
+s3 = boto3.client('s3')
+bucketName = 'bucket-pets'
 def getPets(event, context):
     print(json.dumps({"running": True}))
     print(json.dumps(event))
@@ -20,26 +22,40 @@ def getPets(event, context):
         'statusCode': 200,
         'body': json.dumps("Hello from get Lambda")
     }
+
+def uploadImages(arrayPictures):
+    for picture in arrayPictures:
+        name = str(uuid.uuid1()) +".png"
+        decodeImg = base64.b64decode(picture)
+        s3.put_object(Bucket=bucketName,Key=name,Body=decodeImg)
+        location = s3.get_bucket_location(Bucket=bucketName)['LocationConstraint']
+        objectUrl = "https://%s.s3-%s.amazonaws.com/%s" % (bucketName,location, name)
+        print("This is the url in s3:",objectUrl)
+def updateInfo(pk,sk,health,age,location):
+    response = table.update_item(
+        Key={
+            'pk':pk,
+            'sk':sk
+        },
+        UpdateExpression="set age=:a,healt_status=:h,location=:l",
+        ExpressionAttributeValues={
+            ':a':Numeric(age),
+            ':h':health,
+            ':l':location
+        }
+        ReturnValues="Update_New"
+    )
     
 def updatePet(event, context):
-    print(json.dumps({"running": True}))
-    print(json.dumps(event))
     path = event["path"]
     array_path = path.split("/")
-    user_id =array_path[-1]
+    pet_id =array_path[-1]
     body = event["body"]
-    print(body)
-    # bodyObject = json.loads(body)
-    # table.put_item(
-    #     Item={
-    #         'pk': user_id,
-    #         'sk': 'age',
-    #         'name': bodyObject['name'],
-    #         'last_name': bodyObject['last_name'],
-    #         'age': bodyObject['age']
-    #     }
-    # )
-    
+    bodyObject = json.loads(body)
+    print(bodyObject)
+     
+    if bodyObject["pictures"]:
+        uploadImages(bodyObject["pictures"])
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
