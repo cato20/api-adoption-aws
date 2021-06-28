6e33fedabc624f7cdcdafcd1b85d048dfa6d0376
EC2 Default User <cae-10@hotmail.com>
2021-06-28 03:19:28 +0000

    Added a query for the route of update and fixed the updateInfo.

--- src/pet.py
+++ src/pet.py
@@ -24,39 +24,51 @@ def getPets(event, context):
     }
 
 def uploadImages(arrayPictures):
+    uris=[]
     for picture in arrayPictures:
         name = str(uuid.uuid1()) +".png"
         decodeImg = base64.b64decode(picture)
         s3.put_object(Bucket=bucketName,Key=name,Body=decodeImg)
         location = s3.get_bucket_location(Bucket=bucketName)['LocationConstraint']
         objectUrl = "https://%s.s3-%s.amazonaws.com/%s" % (bucketName,location, name)
+        uris.append(objectUrl)
         print("This is the url in s3:",objectUrl)
-def updateInfo(pk,sk,health,age,location):
+    return uris
+    
+def updateInfo(pk,sk,health,age,locationUpdate,uris):
     response = table.update_item(
         Key={
             'pk':pk,
             'sk':sk
         },
-        UpdateExpression="set age=:a,healt_status=:h,location=:l",
+        UpdateExpression="set age=:a,health_status=:h,#location=:l,pictures=:p",
         ExpressionAttributeValues={
-            ':a':Numeric(age),
-            ':h':health,
-            ':l':location
-        }
-        ReturnValues="Update_New"
+            ':a': age,
+            ':h': health,
+            ':l': locationUpdate,
+            ':p': uris
+        },
+        ExpressionAttributeNames={
+            '#location':'l'
+        },
+        ReturnValues="UPDATED_NEW"
     )
-    
+    return response
 def updatePet(event, context):
+    message=""
     path = event["path"]
     array_path = path.split("/")
+    sk=event["queryStringParameters"]["type"]
     pet_id =array_path[-1]
-    body = event["body"]
-    bodyObject = json.loads(body)
-    print(bodyObject)
-     
+    bodyObject = json.loads(event["body"])
+    health_status = bodyObject["health_status"]
+    age = bodyObject["age"]
+    location = bodyObject["location"]
+
     if bodyObject["pictures"]:
-        uploadImages(bodyObject["pictures"])
+        updateUris = uploadImages(bodyObject["pictures"])
+        message=updateInfo(str(pet_id),sk,health_status,age,location,updateUris)
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps("updated movie succeded")
     }
